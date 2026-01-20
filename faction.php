6d497d6e2e280f3b36b0f7ba8d05d3413c7bc005
txpeaceofficer09 <james.mccaughey@gmail.com>
2026-01-19 23:48:12 -0600

    Update faction.php

--- faction.php
+++ faction.php
@@ -5,7 +5,7 @@ $name = !empty($_REQUEST['name']) ? $_REQUEST['name'] : '';
 if ( !empty($name) ) {
 	if ( !file_exists("images/{$name}.png") ) {
 		if ( $fp=fopen("images/{$name}.png", 'w') ) {
-			fputs($fp, join('', file("https://wow.zamimg.com/images/icons/{$name}.png")));
+			fputs($fp, file_get_contents("https://wow.zamimg.com/images/icons/{$name}.png"));
 			fclose($fp);
 		}
 	}
