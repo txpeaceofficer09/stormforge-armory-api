74a1454169418fb401fd48f4650bb26aa9010a5b
txpeaceofficer09 <james.mccaughey@gmail.com>
2026-01-19 21:17:41 -0600

    Create image.php

---
+++ image.php
@@ -0,0 +1,21 @@
+<?php
+
+$name = !empty($_REQUEST['name']) ? $_REQUEST['name'] : '';
+$src = "https://static.tauri.hu/images/icons/large/{$name}.png";
+$filename = "images/{$name}.png";
+
+if ( !is_dir("images/") ) mkdir("images/", 0755);
+
+if ( !empty($name) && !file_exists($filename) ) {
+	if ( $fp=fopen($filename, 'w') ) {
+		fputs($fp, file_get_contents($src));
+		fclose($fp);
+	}
+}
+
+if ( file_exists($filename) ) {
+	header("Content-Type: image/png");
+	echo file_get_contents($filename);
+}
+
+?>
